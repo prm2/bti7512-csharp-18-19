03d9413306af41f966cb8602e116221eda123733
DESKTOP-0S3IR8B\prm1 <prm1@bfh.ch>
2018-11-01 15:31:14 +0100

    ExGrouping / GroupingWeb

---
+++ 04 linq/ExGrouping/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExGrouping
+{
+    class Program
+    {
+        static void GroupedAlpha()
+        {
+            using (var data = new PersonEntities())
+            {
+                var res = data.People
+                    //.OrderBy(p => p.Name)
+                    //.ThenBy(p => p.Firstname)
+                    .GroupBy(
+                        p => p.Name.Substring(0, 1), 
+                        p => p, 
+                        (key, people) => new { Key = key, People = people });
+                foreach (var r in res)
+                {
+                    Console.WriteLine(r.Key);
+                    Console.ReadLine();
+                    foreach (var p in r.People)
+                        Console.WriteLine("\t{0} {1}", p.Name, p.Firstname);
+                }
+            }
+        }
+
+        static void PagedByFirstChar()
+        {
+            using (var data = new PersonEntities())
+            {
+                var ch = "A";
+                while (ch[0] <= 'Z')
+                {
+                    var res = data.People
+                        .OrderBy(p => p.Name).ThenBy(p => p.Firstname)
+                        .AsEnumerable()
+                        .SkipWhile(p => p.Name.Substring(0,1) != ch)
+                        .TakeWhile(p => p.Name.Substring(0,1) == ch)
+                        .ToList();
+                    Console.WriteLine(ch);
+                    Console.ReadLine();
+                    foreach (var p in res)
+                        Console.WriteLine("\t{0} {1}", p.Name, p.Firstname);
+                    ch = ((char)(ch[0] + 1)).ToString();
+                }
+            }
+        }
+
+        static void PagedByFirstChar2()
+        {
+            using (var data = new PersonEntities())
+            {
+                var ch = "A";
+                while (ch[0] <= 'Z')
+                {
+                    var res = data.People
+                        .Where(p => p.Name.Substring(0, 1) == ch)
+                        .OrderBy(p => p.Name).ThenBy(p => p.Firstname);
+                    Console.WriteLine(ch);
+                    Console.ReadLine();
+                    foreach (var p in res)
+                        Console.WriteLine("\t{0} {1}", p.Name, p.Firstname);
+                    ch = ((char)(ch[0] + 1)).ToString();
+                }
+            }
+        }
+
+        const int PAGE_SIZE = 100;
+
+        static void PagedByCount()
+        {
+            using (var data = new PersonEntities())
+            {
+                var first = 0;
+                var len = 1;
+                while (len > 0)
+                {
+                    var res = data.People
+                        .OrderBy(p => p.Name).ThenBy(p => p.Firstname)
+                        .Skip(first).Take(PAGE_SIZE);
+                    len = res.Count();
+                    if (len > 0)
+                    {
+                        Console.Write(first);
+                        first = first + len;
+                        Console.WriteLine(" - {0}", first - 1);
+                        Console.ReadLine();
+                        foreach (var p in res)
+                            Console.WriteLine("\t{0} {1}", p.Name, p.Firstname);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //GroupedAlpha();
+            //PagedByFirstChar();
+            //PagedByFirstChar2();
+            PagedByCount();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 04 linq/GroupingWeb/Controllers/PeopleController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GroupingWeb.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public ActionResult Char(string id)
+        {
+            using (var data = new GroupingWeb.Models.GroupingEntities())
+            {
+                StringBuilder dblog = new StringBuilder();
+                data.Database.Log = s => dblog.Append(s);
+
+                ViewBag.Char = ' ';
+                ViewBag.DBLog = "";
+
+                if (String.IsNullOrEmpty(id))
+                    return View();
+
+                var ch = id.ToUpper().Substring(0, 1);
+
+                var res = data.People
+                    .Where(p => p.Name.Substring(0, 1) == ch)
+                    .OrderBy(p => p.Name).ThenBy(p => p.Firstname)
+                    .ToList();
+
+                ViewBag.Char = ch;
+                ViewBag.DBLog = dblog.ToString();
+
+                return View(res);
+            }
+        }
+
+        const int PAGE_SIZE = 100;
+
+        public ActionResult Page(int? id)
+        {
+            using (var data = new GroupingWeb.Models.GroupingEntities())
+            {
+                StringBuilder dblog = new StringBuilder();
+                data.Database.Log = s => dblog.Append(s);
+
+                var page = id ?? 1;
+                var count = data.People.Count();
+                var nbOfPages = (count - 1) / PAGE_SIZE + 1;
+
+                var res = data.People
+                    .OrderBy(p => p.Name).ThenBy(p => p.Firstname)
+                    .Skip((page - 1) * PAGE_SIZE).Take(PAGE_SIZE).ToList();
+
+                ViewBag.Page = page;
+                ViewBag.NbOfPages = nbOfPages;
+                ViewBag.DBLog = dblog.ToString();
+
+                return View(res);
+            }
+        }
+    }
+}
