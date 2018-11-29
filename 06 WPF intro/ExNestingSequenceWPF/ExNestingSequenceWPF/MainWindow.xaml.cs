2f0b5b1d3bbbc2cc146492194e486f5208e45ffc
DESKTOP-0S3IR8B\prm1 <prm1@bfh.ch>
2018-11-29 15:29:18 +0100

    WPF: ExNestingSequence

---
+++ 06 WPF intro/ExNestingSequenceWPF/ExNestingSequenceWPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExNestingSequenceWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void b1_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            b1.Opacity = (b1.Opacity < 0.1) ? 1.0 : b1.Opacity - 0.25;
+            t1.Text = $"Text before / Opacity = {b1.Opacity}";
+        }
+
+        private void b2_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            b2.Opacity = (b2.Opacity < 0.1) ? 1.0 : b2.Opacity - 0.25;
+            t2.Text = $"Text inside / Opacity = {b2.Opacity}";
+        }
+
+        private void b3_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            b3.Opacity = (b3.Opacity < 0.1) ? 1.0 : b3.Opacity - 0.25;
+            t3.Text = $"Text after / Opacity = {b3.Opacity}";
+        }
+    }
+}
