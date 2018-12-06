17d39c5908be340692258d5a6edf0327db0b0c07
DESKTOP-0S3IR8B\prm1 <prm1@bfh.ch>
2018-12-06 14:44:32 +0100

    ExGrid

---
+++ 06 WPF intro/ExGrid/ExGrid/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Button btn = new Button();
+            btn.Content = DateTime.Now.ToString();
+            btn.Margin = new Thickness(20);
+            var rand = new Random();
+            btn.Background = new SolidColorBrush(Color.FromRgb(
+                (byte)rand.Next(255), (byte)rand.Next(255), (byte)rand.Next(255)));
+            btn.Click += Button_Click;
+            Grid.Children.Add(btn);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn != null)
+            {
+                var col = Grid.GetColumn(btn);
+                var row = Grid.GetRow(btn);
+
+                if (col > row)
+                    row++;
+                else
+                    col++;
+
+                if (col > 2 || row > 2)
+                    Grid.Children.Remove(btn);
+                else
+                {
+                    Grid.SetColumn(btn, col);
+                    Grid.SetRow(btn, row);
+                }
+            }
+        }
+    }
+}
