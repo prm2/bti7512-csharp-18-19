fe3adf35e180387281a25c6f693b7cfef540d4fb
DESKTOP-0S3IR8B\prm1 <prm1@bfh.ch>
2018-12-13 17:28:37 +0100

    Model classes for MySchedule exercise

---
+++ 08 WPF databinding/MySchedule/Models/Branch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchedule.Models
+{
+    public class Branch
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public static Branch Info = new Branch
+        {
+            Id = "I",
+            Name = "Informatik"
+        };
+        public static Branch MedInf = new Branch
+        {
+            Id = "X",
+            Name = "Medizininformatik"
+        };
+        public static Branch Wing = new Branch 
+        {
+            Id = "W",
+            Name = "Wirtschaftsingenieurwesen"
+        };
+        public static Branch MSE = new Branch 
+        {
+            Id = "MSE",
+            Name = "Master of Science in Engineering"
+        };
+}
+}
---
+++ 08 WPF databinding/MySchedule/Models/Module.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySchedule.Models
+{
+    public enum ModuleType { Mandatory, Project, Option };
+
+    public class Module
+    {
+        public string Title { get; set; }
+        public ModuleType Type { get; set; }
+        public string Class { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public Branch Branch { get; set; }
+        public Location Location { get; set; }
+        public string Room { get; set; }
+    }
+}
