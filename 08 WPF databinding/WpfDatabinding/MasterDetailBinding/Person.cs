0e2e43c7ad7594a37e4c535407594b0d7d001433
DESKTOP-0S3IR8B\prm1 <prm1@bfh.ch>
2018-12-13 16:12:53 +0100

    Databinding examples

---
+++ 08 WPF databinding/WpfDatabinding/MasterDetailBinding/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDetailBinding
+{
+    public class PersonList : ObservableCollection<Person>
+    { }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string FirstName { get; set; }
+    }
+}
---
+++ 08 WPF databinding/WpfDatabinding/ObjectBinding/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectBinding
+{
+    public class Person : INotifyPropertyChanged
+    {
+        string name;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                this.name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var propertyChanged = PropertyChanged;
+            if (propertyChanged != null)
+                propertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
